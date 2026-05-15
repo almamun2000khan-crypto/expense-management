007b3fb66c661bd0d229438103a4f13da277c501
almamun <almamun2000khan@gmail.com>
2026-05-15 20:38:18 +0600

    fix: updated backend url for production

--- expense-management/frontend/src/lib/axiosInstance.js
+++ expense-management/frontend/src/lib/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const backendUrl = import.meta.env.VITE_BACKEND_URL || 'http://localhost:8000/api/v1/';
+const backendUrl = import.meta.env.VITE_API_BASE_URL || 'https://expense-management-2-rpqv.onrender.com/api/v1';
 
 const axiosInstance = axios.create({
   baseURL: backendUrl,
